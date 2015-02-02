fbe570e38236988133befed4cadce48510c88542
kuty <kutyuit@gmail.com>
2015-02-02 12:15:33 +0700

    update database heroku app

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
 var db = {
 	username 		: "admin",
 	passwd 			: "123456789",
-	address 		: "localhost",
-	db_name 		: "khoaluan",
+	address 		: "ds039421.mongolab.com:39421/heroku_app33652451",
+	db_name 		: "heroku_app33652451",
 	db_temp_name 	: "khoaluan_temp",
-	port			: 23892,
+	port			: 39421,
 }
 module.exports = db;
